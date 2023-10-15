c7a35c22276910239d4319b91fc5ae92f576430b
alexandar1vanov <95652613+alexandar1vanov@users.noreply.github.com>
2023-10-15 17:29:05 +0200

    Create math_b2b_operations.cpp

---
+++ math_b2b_operations.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+    //ime(123);
+    cout<<pow(12, 2);
+    sqrt(144);
+    exp(x); e^x
+    log(x);
+    log10(x);
+    abs(-10);
+    
+    floor(10.5);
+    ceil(10.5);
+    
+    sin()
+    cos() 
+    tan()
+}
+
